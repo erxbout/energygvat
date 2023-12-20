c845f795e6e52b96de3da936b8d1311841862c9d
ErXBout <admin@erxbout.ddns.net>
2023-12-20 15:50:47 +0100

    InitialCommit

---
+++ custom_components/energygvat/config_flow.py
@@ -0,0 +1,93 @@
+"""Config flow for energygvat integration."""
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResult
+from homeassistant.exceptions import HomeAssistantError
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+labels = ["Wasser", "Sonne", "Wind", "Biomasse", "Erdgas", "Nettoimporte", "Sonstige"]
+
+
+class PlaceholderHub:
+    """Placeholder class to make tests pass.
+
+    TODO Remove this placeholder class and replace with things from your PyPI package.
+    """
+
+    def __init__(self, host: str) -> None:
+        """Initialize."""
+        self.host = host
+
+    async def authenticate(self, username: str, password: str) -> bool:
+        """Test if we can authenticate with the host."""
+        return True
+
+
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+    """Validate the user input allows us to connect.
+
+    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
+    """
+
+    # Return info that you want to store in the config entry.
+    return {"title": "Energy.Gv.AT"}
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for energygvat."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the initial step."""
+        errors: dict[str, str] = {}
+        if user_input is not None:
+            print(user_input)
+            try:
+                info = await validate_input(self.hass, user_input)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except InvalidAuth:
+                errors["base"] = "invalid_auth"
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                return self.async_create_entry(title=info["title"], data=user_input)
+
+        data_schema = vol.Schema(
+            {
+                vol.Required("PollingInterval"): int,
+                vol.Optional(labels[0]): bool,
+                vol.Optional(labels[1]): bool,
+                vol.Optional(labels[2]): bool,
+                vol.Optional(labels[3]): bool,
+                vol.Optional(labels[4]): bool,
+                vol.Optional(labels[5]): bool,
+                vol.Optional(labels[6]): bool,
+            }
+        )
+
+        return self.async_show_form(
+            step_id="user", data_schema=data_schema, errors=errors
+        )
+
+
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(HomeAssistantError):
+    """Error to indicate there is invalid auth."""
---
+++ custom_components/energygvat/sensor.py
@@ -0,0 +1,166 @@
+import requests
+
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity, StateType
+
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
+from homeassistant.core import callback
+from homeassistant.exceptions import ConfigEntryAuthFailed
+
+from decimal import Decimal
+from datetime import timedelta, date, datetime
+import logging
+
+import async_timeout
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+apiEndpoint = "https://energie.gv.at/api/gd/store/renewable_electricity_now_bars.json"
+labels = {
+    "Wasser",
+    "Sonne",
+    "Wind",
+    "Biomasse",
+    "Erdgas",
+    "Nettoimporte",
+    "Sonstige",
+    "Nichtvorhanden",
+}
+
+
+async def async_setup_platform(
+    hass: HomeAssistant, async_add_entities: AddEntitiesCallback
+) -> None:
+    async_add_entities(EnergyProductionSensor(), True)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    coordinator = MyCoordinator(hass, apiEndpoint, config_entry.data["PollingInterval"])
+
+    await coordinator.async_config_entry_first_refresh()
+
+    entities = []
+
+    for x in config_entry.data:
+        for label in labels:
+            if label == x and config_entry.data[x] == True:
+                entities.append(EnergyProductionSensor(coordinator, label))
+
+    async_add_entities(entities)
+
+
+class MyCoordinator(DataUpdateCoordinator):
+    """My custom coordinator."""
+
+    def __init__(self, hass, apiEndpoint, pollingInterval):
+        """Initialize my coordinator."""
+        super().__init__(
+            hass,
+            # Name of the data. For logging purposes.
+            _LOGGER,
+            name="My sensor",
+            # Polling interval. Will only be polled if there are subscribers.
+            update_interval=timedelta(seconds=pollingInterval * 60),
+        )
+        self.apiEndpoint = apiEndpoint
+        self.hass = hass
+
+    async def _async_update_data(self):
+        """Fetch data from API endpoint.
+
+        This is the place to pre-process the data to lookup tables
+        so entities can quickly look up their data.
+        """
+        try:
+            # Note: asyncio.TimeoutError and aiohttp.ClientError are already
+            # handled by the data update coordinator.
+            async with async_timeout.timeout(10):
+                # Grab active context variables to limit data required to be fetched from API
+                # Note: using context is not required if there is no need or ability to limit
+                # data retrieved from API.
+                listening_idx = set(self.async_contexts())
+                return await self.hass.async_add_executor_job(self.update)
+        # except ApiAuthError as err:
+        # Raising ConfigEntryAuthFailed will cancel future updates
+        # and start a config flow with SOURCE_REAUTH (async_step_reauth)
+        # raise ConfigEntryAuthFailed from err
+        # except ApiError as err:
+        # raise UpdateFailed(f"Error communicating with API: {err}")
+        finally:
+            print("finally hi")
+
+    def update(self) -> None:
+        resp = requests.get(self.apiEndpoint)
+        resp_dict = resp.json()
+
+        return resp_dict
+
+
+class EnergyProductionSensor(CoordinatorEntity, SensorEntity):
+    """An entity using CoordinatorEntity.
+
+    The CoordinatorEntity class provides:
+      should_poll
+      async_update
+      async_added_to_hass
+      available
+
+    """
+
+    timestamp: StateType | date | datetime | Decimal = None
+
+    def __init__(self, coordinator, label):
+        """Pass coordinator to CoordinatorEntity."""
+        super().__init__(coordinator, context=label)
+        self._name = "EnergyGridAT_" + label
+        self._label = label
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        updateDone = False
+
+        for x in self.coordinator.data:
+            if x["label"] == self._label:
+                updateDone = True
+                self._state = round(x["percentage"], 2)
+                self.timestamp = x["date"]
+
+        if updateDone == False:
+            self._state = 0
+
+        print(self._state)
+        print(self._label)
+        self.async_write_ha_state()
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the name of the sensor."""
+        return self._state
+
+    @property
+    def extra_state_attributes(self):
+        """Return the name of the sensor."""
+        result = {"date": self.timestamp}
+
+        return result
+
+    @property
+    def suggested_display_precision(self):
+        return 2
